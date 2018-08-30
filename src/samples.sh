716ca25bbb2879a9abb7a1041d1a193ebb0e15b6
Denis Isidoro <denis_isidoro@live.com>
2018-08-30 12:37:09 -0300

    Remove unnecessary files

--- src/samples.sh
+++
@@ -1,6 +0,0 @@
-spl_usage="$(echo -e "ship move <x> <y>
-ship (create|destroy)
-ship [style] construct")"
-
-spl_opts="$(echo -e "-f --format <format>  Something related to format [default: 2]
---preview -p\n-o --output  asodinasodi [default: 5]")"
